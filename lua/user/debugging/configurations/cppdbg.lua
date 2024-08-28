767f76e76dcc8e1f22fafb1f6bfe8c34e74997a8
Max Sydney <m.sydney@rockatlabusa.com>
2024-08-27 18:36:57 -0700

    Make cppdbg configuration user agnostic

--- lua/user/debugging/configurations/cppdbg.lua
+++ lua/user/debugging/configurations/cppdbg.lua
@@ -1,7 +1,6 @@
 -- Configuration set with VS code launch.json
-
 require("dap").adapters.cppdbg = {
     id = 'cppdbg',
     type = 'executable',
-    command = '/home/max/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+    command = string.format("%s/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7", os.getenv("HOME")),
 }
